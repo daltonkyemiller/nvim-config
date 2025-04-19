93efc67680dc6a526cfee483bfebc71161f1a5a5
Dalton Kye Miller <dalton.kye.miller@gmail.com>
2025-04-19 11:21:34 -0500

    refactor: reorder language injections in scm files

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -37,7 +37,7 @@
                    (#set! injection.language "python"))
         
 ;query
-;; comment typescript injection
+;; comment css injection
 ((comment)
  @comment .
  (lexical_declaration
@@ -48,24 +48,8 @@
              (call_expression(template_string(string_fragment)@injection.content))
              ]@injection.content)  
    )
-  (#match? @comment "^//+( )*[tT][yY][pP][eE][sS][cC][rR][iI][pP][tT]( )*")
-  (#set! injection.language "typescript")
- )
-        
-;query
-;; comment javascript injection
-((comment)
- @comment .
- (lexical_declaration
-   (variable_declarator 
-     value: [
-             (string(string_fragment)@injection.content) 
-             (template_string(string_fragment)@injection.content)
-             (call_expression(template_string(string_fragment)@injection.content))
-             ]@injection.content)  
-   )
-  (#match? @comment "^//+( )*[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]( )*")
-  (#set! injection.language "javascript")
+  (#match? @comment "^//+( )*[cC][sS][sS]( )*")
+  (#set! injection.language "css")
  )
         
 ;query
@@ -82,38 +66,6 @@
    )
   (#match? @comment "^//+( )*lua( )*")
   (#set! injection.language "lua")
- )
-        
-;query
-;; comment python injection
-((comment)
- @comment .
- (lexical_declaration
-   (variable_declarator 
-     value: [
-             (string(string_fragment)@injection.content) 
-             (template_string(string_fragment)@injection.content)
-             (call_expression(template_string(string_fragment)@injection.content))
-             ]@injection.content)  
-   )
-  (#match? @comment "^//+( )*[pP][yY][tT][hH][oO][nN]( )*")
-  (#set! injection.language "python")
- )
-        
-;query
-;; comment css injection
-((comment)
- @comment .
- (lexical_declaration
-   (variable_declarator 
-     value: [
-             (string(string_fragment)@injection.content) 
-             (template_string(string_fragment)@injection.content)
-             (call_expression(template_string(string_fragment)@injection.content))
-             ]@injection.content)  
-   )
-  (#match? @comment "^//+( )*[cC][sS][sS]( )*")
-  (#set! injection.language "css")
  )
         
 ;query
@@ -146,5 +98,53 @@
    )
   (#match? @comment "^//+( )*[sS][qQ][lL]( )*")
   (#set! injection.language "sql")
+ )
+        
+;query
+;; comment javascript injection
+((comment)
+ @comment .
+ (lexical_declaration
+   (variable_declarator 
+     value: [
+             (string(string_fragment)@injection.content) 
+             (template_string(string_fragment)@injection.content)
+             (call_expression(template_string(string_fragment)@injection.content))
+             ]@injection.content)  
+   )
+  (#match? @comment "^//+( )*[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]( )*")
+  (#set! injection.language "javascript")
+ )
+        
+;query
+;; comment typescript injection
+((comment)
+ @comment .
+ (lexical_declaration
+   (variable_declarator 
+     value: [
+             (string(string_fragment)@injection.content) 
+             (template_string(string_fragment)@injection.content)
+             (call_expression(template_string(string_fragment)@injection.content))
+             ]@injection.content)  
+   )
+  (#match? @comment "^//+( )*[tT][yY][pP][eE][sS][cC][rR][iI][pP][tT]( )*")
+  (#set! injection.language "typescript")
+ )
+        
+;query
+;; comment python injection
+((comment)
+ @comment .
+ (lexical_declaration
+   (variable_declarator 
+     value: [
+             (string(string_fragment)@injection.content) 
+             (template_string(string_fragment)@injection.content)
+             (call_expression(template_string(string_fragment)@injection.content))
+             ]@injection.content)  
+   )
+  (#match? @comment "^//+( )*[pP][yY][tT][hH][oO][nN]( )*")
+  (#set! injection.language "python")
  )
         
