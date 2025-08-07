af0ee4747783738a03c97ea22051a03af4b6741b
Dalton Kye Miller <hello@daltons.email>
2025-08-07 13:18:15 -0500

    fix: injections

--- after/queries/typescript/injections.scm
+++ after/queries/typescript/injections.scm
@@ -1,43 +1,7 @@
 ;extends
 
-; query
-;; string sql injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)*-{2,}( )*[sS][qQ][lL]")
-                   (#set! injection.language "sql"))
-        
-; query
-;; string javascript injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)*/{2,}( )*[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]")
-                   (#set! injection.language "javascript"))
-        
-; query
-;; string typescript injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)//+( )*[tT][yY][pP][eE][sS][cC][rR][iI][pP][tT]")
-                   (#set! injection.language "typescript"))
-        
-; query
-;; string html injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)\\<\\!-{2,}( )*[hH][tT][mM][lL]( )*-{2,}\\>")
-                   (#set! injection.language "html"))
-        
-; query
-;; string css injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)/\\*+( )*[cC][sS][sS]( )*\\*+/")
-                   (#set! injection.language "css"))
-        
-; query
-;; string python injection
-((string_fragment) @injection.content
-                   (#match? @injection.content "^(\r\n|\r|\n)*#+( )*[pP][yY][tT][hH][oO][nN]")
-                   (#set! injection.language "python"))
-        
 ;query
-;; comment lua injection
+;; comment sql injection
 ((comment)
  @comment .
  (lexical_declaration
@@ -48,8 +12,8 @@
              (call_expression(template_string(string_fragment)@injection.content))
              ]@injection.content)  
    )
-  (#match? @comment "^//+( )*lua( )*")
-  (#set! injection.language "lua")
+  (#match? @comment "^//+( )*sql( )*")
+  (#set! injection.language "sql")
  )
         
 ;query
@@ -66,38 +30,6 @@
    )
   (#match? @comment "^//+( )*[hH][tT][mM][lL]( )*")
   (#set! injection.language "html")
- )
-        
-;query
-;; comment python injection
-((comment)
- @comment .
- (lexical_declaration
-   (variable_declarator 
-     value: [
-             (string(string_fragment)@injection.content) 
-             (template_string(string_fragment)@injection.content)
-             (call_expression(template_string(string_fragment)@injection.content))
-             ]@injection.content)  
-   )
-  (#match? @comment "^//+( )*[pP][yY][tT][hH][oO][nN]( )*")
-  (#set! injection.language "python")
- )
-        
-;query
-;; comment css injection
-((comment)
- @comment .
- (lexical_declaration
-   (variable_declarator 
-     value: [
-             (string(string_fragment)@injection.content) 
-             (template_string(string_fragment)@injection.content)
-             (call_expression(template_string(string_fragment)@injection.content))
-             ]@injection.content)  
-   )
-  (#match? @comment "^//+( )*[cC][sS][sS]( )*")
-  (#set! injection.language "css")
  )
         
 ;query
@@ -133,7 +65,7 @@
  )
         
 ;query
-;; comment sql injection
+;; comment python injection
 ((comment)
  @comment .
  (lexical_declaration
@@ -144,7 +76,75 @@
              (call_expression(template_string(string_fragment)@injection.content))
              ]@injection.content)  
    )
-  (#match? @comment "^//+( )*sql( )*")
-  (#set! injection.language "sql")
+  (#match? @comment "^//+( )*[pP][yY][tT][hH][oO][nN]( )*")
+  (#set! injection.language "python")
  )
+        
+;query
+;; comment css injection
+((comment)
+ @comment .
+ (lexical_declaration
+   (variable_declarator 
+     value: [
+             (string(string_fragment)@injection.content) 
+             (template_string(string_fragment)@injection.content)
+             (call_expression(template_string(string_fragment)@injection.content))
+             ]@injection.content)  
+   )
+  (#match? @comment "^//+( )*[cC][sS][sS]( )*")
+  (#set! injection.language "css")
+ )
+        
+;query
+;; comment lua injection
+((comment)
+ @comment .
+ (lexical_declaration
+   (variable_declarator 
+     value: [
+             (string(string_fragment)@injection.content) 
+             (template_string(string_fragment)@injection.content)
+             (call_expression(template_string(string_fragment)@injection.content))
+             ]@injection.content)  
+   )
+  (#match? @comment "^//+( )*lua( )*")
+  (#set! injection.language "lua")
+ )
+        
+; query
+;; string sql injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)*-{2,}( )*[sS][qQ][lL]")
+                   (#set! injection.language "sql"))
+        
+; query
+;; string javascript injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)*/{2,}( )*[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]")
+                   (#set! injection.language "javascript"))
+        
+; query
+;; string typescript injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)//+( )*[tT][yY][pP][eE][sS][cC][rR][iI][pP][tT]")
+                   (#set! injection.language "typescript"))
+        
+; query
+;; string html injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)\\<\\!-{2,}( )*[hH][tT][mM][lL]( )*-{2,}\\>")
+                   (#set! injection.language "html"))
+        
+; query
+;; string css injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)/\\*+( )*[cC][sS][sS]( )*\\*+/")
+                   (#set! injection.language "css"))
+        
+; query
+;; string python injection
+((string_fragment) @injection.content
+                   (#match? @injection.content "^(\r\n|\r|\n)*#+( )*[pP][yY][tT][hH][oO][nN]")
+                   (#set! injection.language "python"))
         
